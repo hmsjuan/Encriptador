03d14d15d300824d00220e11a1e92ebf86980fbe
LimaApps <157335145+hmsjuan@users.noreply.github.com>
2024-02-26 19:08:02 -0400

    Cambios

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 
-/*const matriz_claves = [
+const matriz_claves = [
     ['e', 'enter'],
     ['i', 'imes'],
     ['a', 'ai'],
     ['o', 'ober'],
     ['u', 'ufat'],    
-];*/
+];
 
 const mensaje = document.querySelector('#mensaje');
 const resutado_mensaje = document.querySelector("#mensajeResultado");
+ 
 
     /*Definimos los caracteres que permitiremos
         Aunque una de las pautas del reto es no permitir los caracteres especiales
@@ -21,12 +22,13 @@ const resutado_mensaje = document.querySelector("#mensajeResultado");
 el texto y la accion a realizar (e = encriptar y d = desencriptar)*/
 function encriptaDesencripta(accion){   
     let texto = mensaje.value;
-
-    let nuevoTexto=""
-    if(texto.length == 0) {
-        /*Si el texto está vacío salimos de la funcion */
+    let textoResultado="";
+   
+    /*Si el texto está vacío salimos de la funcion */
+    if(texto.length == 0) {       
         asignarTextoElemento('alerta',`&#9888; No hay texto para que sea  ${(accion === 'e') ? 'encriptado' : 'desencriptado'}`);
         setTimeout(limpiarAviso,3000);
+        reiniciarValores();
        return; 
     }
 
@@ -37,64 +39,62 @@ function encriptaDesencripta(accion){
         return;
     }
 
-   
-        /*En cada iteración recorremos el texto y lo comparamos con cada clave
+     /*En cada iteración recorremos el texto y lo comparamos con cada clave
             si coinciden las dos agregamos el valor de la columna a la posición correspondiente
             en el texto y eliminamos la letra de la misma posición en la fila
          */
-    if (accion==="e"){     // e = Encriptar  
-        for (let i=0;i<texto.length;i++) {
-                let caracter = texto[i];
+       
+        if (accion==="e"){     // e = Encriptar   
+            textoResultado = encriptarTexto(texto);       
+        }else if(accion==="d"){ //d = desencriptar
+            textoResultado =  desencriptarTexto(texto)             
+        }  
+                 
 
-                switch (caracter) {
-                    case "a":
-                        nuevoTexto+="ai"
-                        break;
-                            
-                    case "e":
-                        nuevoTexto+="enter"
-                        break;
-
-                    case "i":
-                        nuevoTexto+="imes"
-                        break;
-
-                    case "o":
-                        nuevoTexto+="ober"
-                        break;
-
-                    case "u":
-                        nuevoTexto+="ufat"
-                        break;                
-                                
-                    default:
-                        nuevoTexto+=caracter;
-                        break;
-                }
-        }
-            
-    }else if(accion==="d"){ //d = desencriptar
-                nuevoTexto =   texto.replaceAll("ai", "a")
-                                    .replaceAll("enter", "e")
-                                    .replaceAll("imes", "i")
-                                    .replaceAll("ober", "o")
-                                    .replaceAll("ufat", "u");
-    }
-        
-
-    if (nuevoTexto.valueOf !=""){
+    if (textoResultado.valueOf !=""){
             //Mostramos un mensaje de encriptacion o desencriptacion lograda por 3 segundos en un parrafo 
         
-           asignarTextoElemento('aviso',`&#10004; El texto ha sido  ${(accion === 'e') ? 'encriptado' : 'desencriptado'} correctamente`);
+            asignarTextoElemento('aviso',`&#10004; El texto ha sido  ${(accion === 'e') ? 'encriptado' : 'desencriptado'} correctamente`);
             setTimeout(limpiarAviso,3000);
                     
             //muestro el texto encriptado un texarea y luego lo hago visible
-             asignarTextoElemento("mensajeResultado",texto);    
+             asignarTextoElemento("mensajeResultado",textoResultado);    
             mostrarOcultarElementoDisplay('botonCopiar','block');
             mostrarOcultarElementoDisplay('mensaje-resultado','block');   
             mostrarOcultarElementoDisplay('mensaje-error','none');  
     }
 }
+
+
+function desencriptarTexto(textoEncriptado) {
+    let textoDesencriptado = '';
+    let textoTemporal = textoEncriptado;
+
+    // Recorre la matriz de claves en orden inverso
+    matriz_claves.reverse().forEach(([clave, valor]) => {
+        // Reemplaza cada valor de la matriz en el texto temporal por su clave correspondiente
+        let regex = new RegExp(valor, 'g');
+        textoTemporal = textoTemporal.replace(regex, clave);
+    });
+
+    // El texto desencriptado estará almacenado en textoTemporal
+    return textoTemporal;
+}
+function encriptarTexto(texto) {
+    let textoEncriptado = '';
+    for (let i = 0; i < texto.length; i++) {
+        let letra = texto[i];
+        let clave = matriz_claves.find(([clave]) => clave === letra);
+        if (clave) {
+            textoEncriptado += clave[1];
+        } else {
+            textoEncriptado += letra;
+        }
+    }
+    return textoEncriptado;
+}
+
+
 
 /*Usaremos esta funcion para la cambiar dianmicamente el texto e cualquier elemento HTML
 Tomamos esta funcion del curso anterior, 
@@ -113,22 +113,29 @@ function caracteresEspeciales(texto){
         if(!letrasAceptadas.includes(texto[x]) && texto[x]!==' '){
             contiene = true;
         }   
- }    
-   return  contiene;
+    }    
+    return  contiene;
 }
-
-
     
 function reiniciarValores(){
     //Asignamos los valores por defecto a los campos de entrada y salida
     document.getElementById('mensaje').value=""; 
     asignarTextoElemento('mensajeResultado',''); 
-    mostrarOcultarElemento('botonCopiar','hidden');   
-    mostrarOcultarElemento('resultadoCorrecto','none');   
-    mostrarOcultarElemento('resultadoError','block');  
+    mostrarOcultarElementoDisplay('botonCopiar','none');   
+    mostrarOcultarElementoDisplay('mensaje-resultado','none');   
+    mostrarOcultarElementoDisplay('mensaje-error','block');  
 }
+/*Con esta función haremos que se oculten/muestren algunos elementos dependiendo de ciertas opciones 
+  En este caso usamos el style.display  en block o en none
+*/
+function mostrarOcultarElementoDisplay(elemento, estado){
+     document.getElementById(elemento).style.display = estado;
+ }
 
-/*Con esta función haremos que se oculten/muestren algunos elementos dependiendo de ciertas opciones */
+
+/*Con esta función haremos que se oculten/muestren algunos elementos dependiendo de ciertas opciones
+    En este caso usamos el style.visibility en hidden o en visibility 
+*/
 function mostrarOcultarElemento(elemento, estado){
    document.getElementById(elemento).style.visibility = estado;
 }
@@ -140,8 +147,8 @@ function limpiarAviso(){
 
 
 function copiarTexto(){        
-    
-    document.getElementById("mensajeResultado").select();
+    /* Utilizando la funcion de navegado aplicamos un select sobre el textarea y luego ejecutamos el comando copy en document */
+    resutado_mensaje.select();
 
     try {
         var copiando = document.execCommand('copy');
@@ -152,5 +159,4 @@ function copiarTexto(){
         asignarTextoElemento("alerta","&#9888; El texto no se ha copiado al portapaples");
     }
 
- //   document.body.removeChild(textArea);
-}
+ }
